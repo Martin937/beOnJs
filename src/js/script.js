8ed32b7c333b794ea1349afdb294214caf0ba15f
Martin937 <Martin937@yandex.ru>
2020-10-23 11:18:01 +0300

    lesson2.7

--- src/js/script.js
+++ src/js/script.js
@@ -1,123 +1,25 @@
-// $(document).ready(function() {
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-//         responsive: [{
-//             breakpoint: 992,
-//             settings: {
-//                 dots: true,
-//                 arrows: false
-//             }
-//         }]
-//     });
+// 'use strict';
+var money = prompt("Ваш бюджет на месяц?", '20000'),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '2020-10-21');
 
-//     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-//         $(this)
-//             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-//             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-//     });
+var appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
 
-//     function toggleSlide(item) {
-//         $(item).each(function(i) {
-//             $(this).on('click', function(e) {
-//                 e.preventDefault();
-//                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-//                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-//             })
-//         });
-//     };
+var a1 = prompt("Введите обязательную статью расходов в этом месяце", 'молоко'),
+    a2 = prompt("Во сколько обойдется?", '3000'),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", 'хлеб'),
+    a4 = prompt("Во сколько обойдется?", '3000');
 
-//     toggleSlide('.catalog-item__link');
-//     toggleSlide('.catalog-item__back');
+appData.expenses.a1 = a2;
 
-//     // Modal
-
-//     $('[data-modal=consultation]').on('click', function() {
-//         $('.overlay, #consultation').fadeIn('slow');
-//     });
-//     $('.modal__close').on('click', function() {
-//         $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-//     });
-
-//     $('.button_mini').each(function(i) {
-//         $(this).on('click', function() {
-//             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-//             $('.overlay, #order').fadeIn('slow');
-//         })
-//     });
-
-//     function validateForms(form) {
-//         $(form).validate({
-//             rules: {
-//                 name: {
-//                     required: true,
-//                     minlength: 2
-//                 },
-//                 phone: "required",
-//                 email: {
-//                     required: true,
-//                     email: true
-//                 }
-//             },
-//             messages: {
-//                 name: {
-//                     required: "Пожалуйста, введите свое имя",
-//                     minlength: jQuery.validator.format("Введите {0} символа!")
-//                 },
-//                 phone: "Пожалуйста, введите свой номер телефона",
-//                 email: {
-//                     required: "Пожалуйста, введите свою почту",
-//                     email: "Неправильно введен адрес почты"
-//                 }
-//             }
-//         });
-//     };
-
-//     validateForms('#consultation-form');
-//     validateForms('#consultation form');
-//     validateForms('#order form');
-
-//     $('input[name=phone]').mask("+7 (999) 999-99-99");
-
-//     $('form').submit(function(e) {
-//         e.preventDefault();
-//         $.ajax({
-//             type: "POST",
-//             url: "mailer/smart.php",
-//             data: $(this).serialize()
-//         }).done(function() {
-//             $(this).find("input").val("");
-//             $('#consultation, #order').fadeOut();
-//             $('.overlay, #thanks').fadeIn('slow');
-
-//             $('form').trigger('reset');
-//         });
-//         return false;
-//     });
-
-//     // Smooth scroll and pageup
-
-//     $(window).scroll(function() {
-//         if ($(this).scrollTop() > 1600) {
-//             $('.pageup').fadeIn();
-//         } else {
-//             $('.pageup').fadeOut();
-//         }
-//     });
-
-//     $("a[href=#up]").click(function() {
-//         const _href = $(this).attr("href");
-//         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
-//         return false;
-//     });
-//     $("a[href^='#catalog']").click(function() {
-//         var _href = $(this).attr("href");
-//         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
-//         return false;
-//     });
+appData.expenses.a3 = a4;
 
 
-//     new WOW().init();
-// });
+alert(appData.budget / 30);
+console.log(appData);
--- src/read.md
+++ src/read.md
@@ -9,15 +9,15 @@
 9.	git config --local core.quotepath off	Установка отображения unicode
 10.	git status	Посмотреть состояние
 11.	git add -A	Добавить все файлы
-12.	git commit -a -m”message”	Создание контрольной точки
+12.	git commit -a -m”first-message”	Создание контрольной точки
 13.	git add -A + git commit -a -m”message”	Вводить после внесения всех изменений
 14.	git log	Посмотреть изменения
 15.
 16.
 17.	Создаём репозиторий на GITHUB и связываем его с проектом
 18.
-19.	git remote add origin https://github.com/Martin937//beOnPulse.git	Прописываем путь к github
-20.	git remote set-url origin https://github.com/Martin937/beOnPulse.git	Если нужно изменить уже существующий
+19.	git remote add origin https://github.com/Martin937/beOnJs.git	Прописываем путь к github
+20.	git remote set-url origin https://github.com/Martin937/beOnJs.git	Если нужно изменить уже существующий
 21.	git push -u origin master	Заливка на github
 22.	git pull	Обновление всех связей и изменений
 23.	git push	Заливка на github
